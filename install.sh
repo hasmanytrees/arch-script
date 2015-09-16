d28dc4df05f58e771f929902033213c98b293456
Forrest Robertson <forrest@hasmanytrees.com>
2015-09-15 22:09:21 -0600

    fixed usage of arch-chroot

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ parted ${TARGET} mkpart primary ext4 513MiB 100%
 
 # format the partitions
 mkfs.fat -F32 ${TARGET}1
-mkfs.ext4 ${TARGET}2 -L ArchRoot
+mkfs.ext4 ${TARGET}2
 
 # mount the partitions
 mount ${TARGET}2 /mnt
@@ -26,53 +26,49 @@ pacstrap /mnt base base-devel
 # configure the system
 genfstab -U /mnt > /mnt/etc/fstab
 
-arch-chroot /mnt /bin/bash
+echo "arch" > /mnt/etc/hostname
 
-echo "arch" > /etc/hostname
+echo "en_US.UTF-8 UTF-8" >> /mnt/etc/locale.gen
 
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+arch-chroot /mnt locale-gen
 
-locale-gen
+echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
-ln -sf /usr/share/zoneinfo/UTC /etc/localtime
-
-hwclock --systohc --utc
+arch-chroot /mnt hwclock --systohc --utc
 
 # install bootloader
-bootctl install
+arch-chroot /mnt bootctl install
 
-echo -e "title\tArch Linux" > /boot/loader/entries/arch.conf
-echo -e "linux\t/vmlinuz-linux" >> /boot/loader/entries/arch.conf
-echo -e "initrd\t/initramfs-linux.img" >> /boot/loader/entries/arch.conf
-echo -e "options\troot=LABEL=ArchRoot rw" >> /boot/loader/entries/arch.conf
+echo -e "title\tArch Linux" > /mnt/boot/loader/entries/arch.conf
+echo -e "linux\t/vmlinuz-linux" >> /mnt/boot/loader/entries/arch.conf
+echo -e "initrd\t/initramfs-linux.img" >> /mnt/boot/loader/entries/arch.conf
+echo -e "options\troot=PARTUUID=$(blkid -s PARTUUID -o value ${TARGET}2) rw" >> /mnt/boot/loader/entries/arch.conf
 
-echo -e "timeout\t3" > /boot/loader/loader.conf
-echo -e "default\tarch" >> /boot/loader/loader.conf
+echo -e "timeout\t3" > /mnt/boot/loader/loader.conf
+echo -e "default\tarch" >> /mnt/boot/loader/loader.conf
 
-pacman -S iw wpa_supplicant dialog
+arch-chroot /mnt pacman -S iw wpa_supplicant dialog
 
-pacman -S xorg-server xorg-xinit xorg-drivers
+arch-chroot /mnt pacman -S xorg-server xorg-xinit xorg-drivers
 
-pacman -S gdm gnome-shell gnome-terminal gnome-control-center gnome-keyring network-manager-applet nautilus xdg-user-dirs
+arch-chroot /mnt pacman -S gdm gnome-shell gnome-terminal gnome-control-center gnome-keyring network-manager-applet nautilus xdg-user-dirs
 
-systemctl enable gdm NetworkManager
+arch-chroot /mnt systemctl enable gdm NetworkManager
 
-xdg-user-dirs-update
+arch-chroot /mnt xdg-user-dirs-update
 
 # manage users
-passwd
+arch-chroot /mnt passwd
 
-useradd -m -G wheel -s /bin/bash forrest
+arch-chroot /mnt useradd -m -G wheel -s /bin/bash forrest
 
-passwd forrest
+arch-chroot /mnt passwd forrest
 
-sed -i '/NOPASSWD/!s/# %wheel/%wheel/g' /etc/sudoers
+arch-chroot /mnt sed -i '/NOPASSWD/!s/# %wheel/%wheel/g' /etc/sudoers
 
 # reboot
-exit
-
 umount -R /mnt
 
 reboot
