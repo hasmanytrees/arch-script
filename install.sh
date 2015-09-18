2a546512cae1ccd0cdbf9c75046f05de10a98fee
Forrest Robertson <forrest@hasmanytrees.com>
2015-09-18 01:11:41 -0600

    Fixed sed command for locale.gen

--- install.sh
+++ install.sh
@@ -113,7 +113,7 @@ function os() {
 
 
 function set_locale_timezone() {
-    sed -i 's/#en-US.UTF-8 UTF-8/en-US.UTF-8 UTF-8/' /etc/locale.gen
+    sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
     locale-gen
     echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
