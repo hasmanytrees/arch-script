35234ce88ac8a7655317a51064404989a3b101b6
Forrest Robertson <forrest@hasmanytrees.com>
2015-09-19 13:17:54 -0600

    Added settings for npm and git

--- install.sh
+++ install.sh
@@ -14,7 +14,8 @@ hostname=hostname
 
 root_password=password
 
-
+admin_git_name=name
+admin_git_email=email
 admin_username=username
 admin_password=password
 
@@ -58,7 +59,11 @@ aur_packages+=" atom-editor-bin"
 
 
 setup_apps() {
-  su $admin_username -c "./install.sh setup_npm"
+  su $admin_username -c "/install.sh setup_npm"
+
+  su $admin_username -c "/install.sh setup_git"
+
+  su $admin_username -c "/install.sh setup_gtk"
 }
 
 
@@ -72,9 +77,27 @@ setup_npm() {
 }
 
 
-#setup_git() {
+setup_git() {
+  git config --global user.name "$admin_git_name"
+  git config --global user.email "$admin_git_email"
 
-#}
+  mkdir ~/.ssh
+  ssh-keygen -t rsa -b 4096 -C "$admin_git_email" -N "" -f ~/.ssh/id_rsa
+
+  eval "$(ssh-agent-s)"
+  ssh-add ~/.ssh/id_rsa
+}
+
+
+setup_gtk() {
+  gsettings set org.gnome.shell enabled-extensions "['user-theme@gnome-shell-extensions.gcampax.github.com', 'dash-to-dock@micxgx.gmail.com']"
+
+  gsettings set org.gnome.desktop.interface gtk-theme "Numix-Frost"
+	gsettings set org.gnome.desktop.wm.preferences theme "Numix-Frost"
+	gsettings set org.gnome.shell.extensions.user-theme name "Numix-Frost"
+
+	gsettings set org.gnome.desktop.interface icon-theme "Numix-Square"
+}
 
 ###################################################################################################
 
